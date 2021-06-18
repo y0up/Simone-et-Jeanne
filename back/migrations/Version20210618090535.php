cee2054da4c76a39ad76e0a5a067446cb986f8b7
Y0up <alexis.dangelo.marquine@gmail.com>
2021-06-18 11:07:08 +0200

    make migration

---
+++ back/migrations/Version20210618090535.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20210618090535 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE cart_item DROP INDEX UNIQ_F0FE25274584665A, ADD INDEX IDX_F0FE25274584665A (product_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE cart_item DROP INDEX IDX_F0FE25274584665A, ADD UNIQUE INDEX UNIQ_F0FE25274584665A (product_id)');
+    }
+}
